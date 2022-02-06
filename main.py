77eba1d8a0d713aca18c3ecfb148b2946d8cf719
Gautam Mitra <gmitra15@gmail.com>
2022-02-05 21:11:53 -0500

    let's get started

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Filename: main.py
+Project: WordleBot
+Name: Gautam Mitra
+Last Updated: 02/5/2022
+Purpose: Create an interactive solver for popular online word game Wordle
+"""
+
+def main():
+    print("Step 1")
+
+if __name__ == "__main__":
+    main()
